e3ff42a4513bd0afb75e7a852415161b8190f4e7
zeraati <zeraati@csis.local>
2026-09-20 11:22:04 +0330

    auth/login

--- back/API/Feature/03.Auth/Endpoint/Login/Endpoint.cs
+++ back/API/Feature/03.Auth/Endpoint/Login/Endpoint.cs
@@ -1,46 +1,39 @@
-﻿using API.Feature.Domain;
+﻿namespace API.Feature.Endpoint;
 
-namespace API.Feature.Endpoint;
-
-public class UserSearch : FastEndpoint<UserSearchRequest, UserGetResponse[]>
+public class AuthLogin : FastEndpointWithoutRequest<object>
 {
     public override void Configure()
     {
-        Get("auth/login");
+        Post("auth/login");
     }
 
     public override async Task HandleAsync(CancellationToken cancellation)
     {
-           Response = new LoginResponse
+        Response = new
         {
-            Data = new LoginData
+            Data = new
             {
-                TokenInfo = new TokenInfo
+                TokenInfo = new
                 {
-                    Metadata = new TokenMetadata
+                    Metadata = new
                     {
                         Username = "mortazavi",
                         PersonnelId = 990962
                     },
 
                     JwtToken = "eyJhbGciOiJFUzM4NCIsInR5cCI6IkpXVCJ9...",
-                    
+
                     IssuedOn = DateTime.Parse("2026-09-19T16:36:38"),
                     ExpiresOn = DateTime.Parse("2026-09-19T17:06:38"),
 
                     RefreshToken = "6EBB372034B3215366C41357B8AE3DA16FCCD63D91BC7DD3E9DADF3CF16E762C000CB09FD79C5FA97299DFA1BA5937F884BEA39A1F488A32E547F4B911F6E207",
 
                     PasswordExpired = false,
-                    RequiresTwoFactorAuthentication = false,
-
-                    TwoFactorToken = null,
-                    ErrorMessage = null,
-                    Mobile = null
+                    RequiresTwoFactorAuthentication = false
                 }
             },
 
             Failed = false,
-            Message = null,
             Succeeded = true,
             Code = 0
         };
--- back/API/Feature/03.Auth/Endpoint/Login/Request.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace API.Feature.Endpoint;
-
-public record UserSearchRequest(string? Name);
